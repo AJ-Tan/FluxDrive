eaa222d5a01d7902323c7ceddb7d5edb508bc288
AJ-Tan <aldomin22@gmail.com>
2026-07-20 22:38:23 +0800

    feat: add HeroSection component with styles for responsive layout

---
+++ fluxdrive/src/pages/LandingPage/components/HeroSection/HeroSection.tsx
@@ -0,0 +1,32 @@
+import LinkButton from "../../../../components/Buttons/LinkButton";
+import PageLogo from "../../../../components/PageLogo/PageLogo";
+import heroImg from "../../../../assets/landing-page/hero-image.png";
+import "./heroSection.css";
+
+function HeroSection() {
+  return (
+    <section id="home" className="page-hero">
+      <div className="page-hero-content">
+        <PageLogo />
+        <h1>Store and share files online</h1>
+        <p>
+          Secure cloud storage for seamless file sharing and enhanced
+          collaboration
+        </p>
+        <div className="page-hero-controls">
+          <LinkButton to="/signin" scale={1}>
+            Sign in
+          </LinkButton>
+          <LinkButton to="/signup" variants="secondary" scale={1}>
+            Sign up
+          </LinkButton>
+        </div>
+      </div>
+      <div className="page-hero-image">
+        <img src={heroImg} alt="" />
+      </div>
+    </section>
+  );
+}
+
+export default HeroSection;
