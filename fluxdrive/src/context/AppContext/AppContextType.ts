e8dc6b822576b7340a13721a4f01a4ccd3206dda
AJ-Tan <aldomin22@gmail.com>
2026-08-03 13:28:45 +0800

    feat: refactor AppContext and AppProvider to use a reducer for state management, define AppStateType and AppActionType for improved structure

---
+++ fluxdrive/src/context/AppContext/AppContextType.ts
@@ -0,0 +1,49 @@
+import type { FileType } from "../../types/file-types";
+import type { FolderType } from "../../types/folder-types";
+
+export type AppStateType = {
+  allFolders: FolderType[];
+  allFiles: FileType[];
+  activeFolder: string | null;
+  focusedItem: {
+    id: string;
+    itemType: "folder" | "file";
+  } | null;
+};
+
+export type AppActionType =
+  | {
+      type: "updateData";
+      payload: {
+        folderId?: string;
+        allFolders: FolderType[];
+        allFiles: FileType[];
+      };
+    }
+  | {
+      type: "activeFolder";
+      payload: string;
+    }
+  | {
+      type: "focusedItem";
+      payload: {
+        id: string;
+        itemType: "folder" | "file";
+      };
+    }
+  | {
+      type: "updateFolders";
+      payload: FolderType[];
+    }
+  | {
+      type: "updateFiles";
+      payload: FileType[];
+    }
+  | {
+      type: "clearFocusedItem";
+    };
+
+export type AppReducerType = (
+  state: AppStateType,
+  action: AppActionType,
+) => AppStateType;
--- fluxdrive/src/context/AppContext/AppProvider.tsx
+++ fluxdrive/src/context/AppContext/AppProvider.tsx
@@ -1,42 +1,95 @@
-import { useEffect, useState, type JSX } from "react";
-import {
-  AppContext,
-  type AsideFoldersType,
-  type FocusedItemType,
-  type SelectedFolderType,
-} from "./AppContext";
+import { useEffect, useReducer, type JSX } from "react";
+import { AppContext } from "./AppContext";
 import { folderAll } from "../../services/folder-service";
+import useAuth from "../AuthContext/useAuth";
+import type { AppReducerType } from "./AppContextType";
 
-const asideFoldersInitialState = {
-  id: "0",
-  name: "My Drive",
-  parentId: null,
-  ownerId: "0",
-  createdAt: "0",
-  updatedAt: "0",
-  children: [],
+const allFoldersInitialState = [
+  {
+    id: "0",
+    name: "My Drive",
+    parentId: "",
+    ownerId: "0",
+    createdAt: "0",
+    updatedAt: "0",
+    parent: null,
+    children: [],
+    files: [],
+    folderPath: [],
+  },
+];
+
+const appInitialState = {
+  allFolders: allFoldersInitialState,
+  allFiles: [],
+  activeFolder: null,
+  focusedItem: null,
+};
+
+const appReducer: AppReducerType = (state, action) => {
+  switch (action.type) {
+    case "updateData":
+      return {
+        ...state,
+        allFolders: action.payload.allFolders,
+        allFiles: action.payload.allFiles,
+        activeFolder: action.payload.folderId
+          ? action.payload.folderId
+          : state.activeFolder,
+      };
+    case "activeFolder":
+      return {
+        ...state,
+        activeFolder: action.payload,
+        focusedItem: {
+          id: action.payload,
+          itemType: "folder",
+        },
+      };
+    case "focusedItem":
+      return {
+        ...state,
+        focusedItem: {
+          id: action.payload.id,
+          itemType: action.payload.itemType,
+        },
+      };
+    case "updateFolders":
+      return {
+        ...state,
+        allFolders: action.payload,
+      };
+    case "updateFiles":
+      return {
+        ...state,
+        allFiles: action.payload,
+      };
+    case "clearFocusedItem":
+      return { ...state, focusedItem: null };
+    default:
+      return { ...state };
+  }
 };
 
 function AppProvider({ children }: { children: JSX.Element }) {
-  const [asideFolders, setAsideFolders] = useState<AsideFoldersType>(
-    asideFoldersInitialState,
-  );
-  const focusedItem = useState<FocusedItemType>(null);
-  const selectedFolder = useState<SelectedFolderType>(null);
+  const [appState, dispatch] = useReducer(appReducer, appInitialState);
+  const { user } = useAuth();
 
   useEffect(() => {
     folderAll().then((res) => {
       if (!res.ok) return console.log(res);
-      setAsideFolders(res.data.folder);
+      dispatch({
+        type: "updateData",
+        payload: { folderId: `${user?.id}-base`, ...res.data },
+      });
     });
-  }, [setAsideFolders]);
+  }, [user, dispatch]);
 
   return (
     <AppContext
       value={{
-        asideFolders: [asideFolders, setAsideFolders],
-        focusedItem,
-        selectedFolder,
+        appState,
+        dispatch,
       }}
     >
       {children}
