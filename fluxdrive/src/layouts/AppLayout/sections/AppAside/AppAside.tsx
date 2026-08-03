a6ed3b73202389d2ec72a4e9af6c1bab1d9bbdf6
AJ-Tan <aldomin22@gmail.com>
2026-08-03 13:29:12 +0800

    feat: add ButtonNew component and integrate it into AppAside for folder creation

--- fluxdrive/src/layouts/AppLayout/sections/AppAside/AppAside.tsx
+++ fluxdrive/src/layouts/AppLayout/sections/AppAside/AppAside.tsx
@@ -1,15 +1,47 @@
 import "./appAside.css";
 import useApp from "../../../../context/AppContext/useApp";
 import FolderItem from "./components/FolderItem/FolderItem";
+import ButtonNew from "./components/ButtonNew/ButtonNew";
+import useAuth from "../../../../context/AuthContext/useAuth";
+import type { FolderType } from "../../../../types/folder-types";
 
 function AppAside() {
-  const {
-    asideFolders: [asideFolder],
-  } = useApp();
+  const { appState } = useApp();
+  const { user } = useAuth();
+
+  const structuredAllFolderData = () => {
+    const allFolders = appState.allFolders;
+    if (!user || !allFolders[0]?.id) return allFolders[0];
+
+    const baseFolderId = `${user.id}-base`;
+    const baseFolder = allFolders.filter(
+      (folder) => folder.id === baseFolderId,
+    )[0];
+
+    if (!baseFolder) return allFolders[0];
+    const arrangeFolderData = (folderParamObj: FolderType): FolderType => {
+      const childrenFolders = allFolders.filter(
+        (folder) => folder.parentId === folderParamObj.id,
+      );
+
+      const children: FolderType[] =
+        childrenFolders.map((child) => {
+          return arrangeFolderData(child);
+        }) || [];
+
+      return {
+        ...folderParamObj,
+        children,
+      };
+    };
+
+    return arrangeFolderData(baseFolder);
+  };
 
   return (
     <aside className="app-aside">
-      <FolderItem key={`root`} folder={asideFolder} index={1} />
+      <ButtonNew />
+      <FolderItem key={`root`} folder={structuredAllFolderData()} index={1} />
     </aside>
   );
 }
---
+++ fluxdrive/src/layouts/AppLayout/sections/AppAside/components/ButtonNew/ButtonNew.tsx
@@ -0,0 +1,13 @@
+import "./buttonNew.css";
+
+function ButtonNew() {
+  return (
+    <div className="button-new--container">
+      <button className="button-new">
+        <div className="icon">+</div>New
+      </button>
+    </div>
+  );
+}
+
+export default ButtonNew;
