cf79e9185a8d47e9a8ac0df28ff0a7aa3e3c490b
AJ-Tan <aldomin22@gmail.com>
2026-08-22 18:12:03 +0800

    Add SVG icons for file download and folder share; enhance ErrorPage component and styles
    - Introduced new SVG icons: file-download.svg and folder-share.svg.
    - Updated ErrorPage component to include a new error message prop and a button for navigation.
    - Created errorPage.css for styling the ErrorPage layout and responsiveness.

--- fluxdrive/src/pages/ErrorPage/ErrorPage.tsx
+++ fluxdrive/src/pages/ErrorPage/ErrorPage.tsx
@@ -1,17 +1,56 @@
+import ErrorImage from "../../assets/error-page/error-storyset.svg?react";
+import PageLogo from "../../components/PageLogo/PageLogo";
+import type { ErrorType } from "../../context/AppContext/AppProvider";
+import "./errorPage.css";
+import { useNavigate } from "react-router";
+
 type ErrorPagePropsType = {
+  defaultUrl: string;
   status: number;
-  title: string;
-  description: string;
+  message: string;
+  setError?: React.Dispatch<React.SetStateAction<ErrorType>> | null;
 };
 
-function ErrorPage({ status, title, description }: ErrorPagePropsType) {
+function ErrorPage({
+  defaultUrl,
+  status,
+  message,
+  setError,
+}: ErrorPagePropsType) {
+  const navigate = useNavigate();
+
+  const handleGotoHome = () => {
+    if (setError) {
+      setError(null);
+    }
+    navigate(defaultUrl, { replace: true });
+  };
+
   return (
-    <div>
-      <div className="error-header">
-        <span>
-          <b>{status}</b> {title}
-        </span>
-        <p>{description}</p>
+    <div className="error-page">
+      <div className="error-details">
+        <button
+          type="button"
+          onClick={handleGotoHome}
+          aria-label="Navigate to app."
+          role="link"
+        >
+          <PageLogo />
+        </button>
+        <div className="error-header">
+          <span>
+            <span className="highlight">
+              <b>{status}.</b>
+            </span>{" "}
+            That's an error.
+          </span>
+        </div>
+        <p>
+          <span className="highlight">{message}</span> That's all we know.
+        </p>
+      </div>
+      <div className="error-image">
+        <ErrorImage />
       </div>
     </div>
   );
